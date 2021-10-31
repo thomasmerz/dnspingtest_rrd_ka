713a9f520863df2889be72d151fc067bc1edf3cc
Thomas Merz <git@rhoenwurz.de>
2021-10-31 23:55:33 +0100

    ✨ dnsping also Pi-Hole's default dns resolvers which offer ECS and DNSSEC

--- 98_create_html.sh
+++ 98_create_html.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for what in 192.168.42.241 192.168.0.13 45.90.28.39 45.90.30.39 1.1.1.1 1.0.0.1; do
-  cp -af index_192.168.0.1.html index_$what.html
-  sed -i "s/192.168.0.1/$what/g" index_$what.html
+# for what in 192.168.42.241 192.168.0.13 45.90.28.39 45.90.30.39 1.1.1.1 1.0.0.1; do
+for what in 192.168.42.241 192.168.0.13 45.90.28.39 45.90.30.39 1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4 208.67.222.222 208.67.220.220 84.200.69.80 84.200.70.40  9.9.9.11 149.112.112.11; do
+  cp -af index_192.168.0.13.html index_$what.html
+  sed -i "s/192.168.0.13/$what/g" index_$what.html
   sed -i "s/src=ping_/src=dnsping_/g" index_$what.html
 done
 
