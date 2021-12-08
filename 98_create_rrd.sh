6c92c77cf3a33e7da88a628b853fa2ff08b406ac
Thomas Merz <git@rhoenwurz.de>
2021-12-08 22:45:40 +0100

    🚸 create RRD files in current directory:    98_create_rrd.sh

--- 98_create_rrd.sh
+++ 98_create_rrd.sh
@@ -18,7 +18,7 @@
 
 # for what in 192.168.42.241 192.168.0.13 45.90.28.39 45.90.30.39 1.1.1.1 1.0.0.1; do
 for what in 80.69.96.12 45.90.28.39 45.90.30.39 1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4 208.67.222.222 208.67.220.220 84.200.69.80 84.200.70.40 9.9.9.11 149.112.112.11 192.168.0.1 192.168.42.241 192.168.0.13; do
-  /usr/bin/rrdtool create /home/thomas/Documents/Administration/dnspingtest_rrd/dnsping_${what}.rrd \
+  /usr/bin/rrdtool create dnsping_${what}.rrd \
     --step 60 \
     DS:pl:GAUGE:600:0:100 \
     DS:rtt:GAUGE:600:0:10000000 \
