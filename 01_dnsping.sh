7e6f85090f4386fd85ee6199c0e72d17cd0b245b
Thomas Merz <git@rhoenwurz.de>
2022-03-13 23:06:58 +0100

    🚑 fix defunct script due to too much and blind confidence in shellcheck'ing

--- 01_dnsping.sh
+++ 01_dnsping.sh
@@ -19,7 +19,6 @@ DEADLINE=10
 
 dnsping_host() {
     output="$($PING -q -c $COUNT -w $DEADLINE -s "$1" nextwurz.mooo.com 2>&1)"
-    local output
     # notice $output is quoted to preserve newlines
     temp=$(echo "$output"| awk '
         BEGIN           {pl=100; rtt=0.1}
