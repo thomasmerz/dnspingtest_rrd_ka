d2fef1c5b8b78d8a8df0a58fec050f5c517307e8
Thomas Merz <git@rhoenwurz.de>
2023-04-20 15:44:26 +0200

    Issue 1 ♻️  refactor scripts and shellcheck them:
       🎨 01_dnsping.sh⏎
       🎨 02_dnsping-graph.sh⏎
       🔥 98_create_html.sh⏎
       🔥 98_create_rrd.sh⏎
       ✨ dnsresolvers.list
    
    ✨ Add new DNS resolvers from 1Hosts to be monitored

--- 02_dnsping-graph.sh
+++ 02_dnsping-graph.sh
@@ -9,21 +9,37 @@ case $(hostname) in
     cd ~/dev/dnspingtest_rrd."$(hostname)"/ || exit 1
     ;;
   *)
+    echo "Please add '$(hostname)' in 'case' block in this script: $0."
     exit 1
     ;;
 esac
 
-h='80.69.96.12 45.90.28.39 45.90.30.39 1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4 208.67.222.222 208.67.220.220 84.200.69.80 84.200.70.40 9.9.9.11 149.112.112.11 192.168.0.1 192.168.42.241 192.168.0.13 46.182.19.48 194.150.168.168 5.1.66.255 185.150.99.255 80.241.218.68 159.69.114.157 176.9.93.198 176.9.1.117 94.140.14.14 94.140.15.15 94.140.14.140 94.140.14.141 95.215.19.53 185.253.5.0 193.110.81.0 193.110.81.9 185.253.5.9'
-for what in $h; do
+# -- MAIN --
 
-  rrdtool graph images/dnsping_"${what}"_hour.png -h 500 -w 1200 -a PNG \
+if [ ! -f dnsresolvers.list ]; then
+  echo "dnsresolvers.list: file not found."
+  exit 2
+fi
+resolverlist="$(grep -v ^\# dnsresolvers.list)"
+[ -z "$resolverlist" ] && exit 1
+for resolver in $resolverlist; do
+
+  # create html-file from "template" if not existing:
+  for file in index_"$resolver"*.html; do
+    if [ ! -e "$file" ]; then
+      cp -af index_192.168.0.13_pihole_wlan.html index_"$resolver".html
+      sed -i "s/192.168.0.13/$resolver/g" index_"$resolver".html
+    fi
+  done
+
+  rrdtool graph images/dnsping_"${resolver}"_hour.png -h 500 -w 1200 -a PNG \
   --imginfo '<IMG SRC=/stats/%s WIDTH=%lu HEIGHT=%lu >' \
   --start -3600 --end -60 --x-grid MINUTE:10:HOUR:1:MINUTE:30:0:%R \
   -v "Response Time (ms)" \
   --rigid \
   --lower-limit 0 \
-  DEF:roundtrip=data/dnsping_"${what}".rrd:rtt:AVERAGE \
-  DEF:packetloss=data/dnsping_"${what}".rrd:pl:AVERAGE \
+  DEF:roundtrip=data/dnsping_"${resolver}".rrd:rtt:AVERAGE \
+  DEF:packetloss=data/dnsping_"${resolver}".rrd:pl:AVERAGE \
   CDEF:PLNone=packetloss,0,2,LIMIT,UN,UNKN,INF,IF \
   CDEF:PL2=packetloss,2,8,LIMIT,UN,UNKN,INF,IF \
   CDEF:PL15=packetloss,8,15,LIMIT,UN,UNKN,INF,IF \
@@ -45,14 +61,14 @@ for what in $h; do
   AREA:PL100#ff0000:"75-100%":STACK \
   COMMENT:"(Packet Loss Percentage)"
 
-  rrdtool graph images/dnsping_"${what}"_day.png -h 500 -w 1200 -a PNG \
+  rrdtool graph images/dnsping_"${resolver}"_day.png -h 500 -w 1200 -a PNG \
   --imginfo '<IMG SRC=/stats/%s WIDTH=%lu HEIGHT=%lu >' \
   --lazy --start -86400 --end -60 --x-grid MINUTE:30:HOUR:1:HOUR:2:0:%H \
   -v "Response Time (ms)" \
   --rigid \
   --lower-limit 0 \
-  DEF:roundtrip=data/dnsping_"${what}".rrd:rtt:AVERAGE \
-  DEF:packetloss=data/dnsping_"${what}".rrd:pl:AVERAGE \
+  DEF:roundtrip=data/dnsping_"${resolver}".rrd:rtt:AVERAGE \
+  DEF:packetloss=data/dnsping_"${resolver}".rrd:pl:AVERAGE \
   CDEF:PLNone=packetloss,0,2,LIMIT,UN,UNKN,INF,IF \
   CDEF:PL2=packetloss,2,8,LIMIT,UN,UNKN,INF,IF \
   CDEF:PL15=packetloss,8,15,LIMIT,UN,UNKN,INF,IF \
@@ -74,14 +90,14 @@ for what in $h; do
   AREA:PL100#ff0000:"75-100%":STACK \
   COMMENT:"(Packet Loss Percentage)"
 
-  rrdtool graph images/dnsping_"${what}"_week.png -h 500 -w 1200 -a PNG \
+  rrdtool graph images/dnsping_"${resolver}"_week.png -h 500 -w 1200 -a PNG \
   --imginfo '<IMG SRC=/stats/%s WIDTH=%lu HEIGHT=%lu >' \
   --lazy --start -604800 --end -1800 \
   -v "Response Time (ms)" \
   --rigid \
   --lower-limit 0 \
-  DEF:roundtrip=data/dnsping_"${what}".rrd:rtt:AVERAGE \
-  DEF:packetloss=data/dnsping_"${what}".rrd:pl:AVERAGE \
+  DEF:roundtrip=data/dnsping_"${resolver}".rrd:rtt:AVERAGE \
+  DEF:packetloss=data/dnsping_"${resolver}".rrd:pl:AVERAGE \
   CDEF:PLNone=packetloss,0,2,LIMIT,UN,UNKN,INF,IF \
   CDEF:PL2=packetloss,2,8,LIMIT,UN,UNKN,INF,IF \
   CDEF:PL15=packetloss,8,15,LIMIT,UN,UNKN,INF,IF \
@@ -103,14 +119,14 @@ for what in $h; do
   AREA:PL100#ff0000:"75-100%":STACK \
   COMMENT:"(Packet Loss Percentage)"
 
-  rrdtool graph images/dnsping_"${what}"_month.png -h 500 -w 1200 -a PNG \
+  rrdtool graph images/dnsping_"${resolver}"_month.png -h 500 -w 1200 -a PNG \
   --imginfo '<IMG SRC=/stats/%s WIDTH=%lu HEIGHT=%lu >' \
   --lazy --start -2592000 --end -7200 \
   -v "Response Time (ms)" \
   --rigid \
   --lower-limit 0 \
-  DEF:roundtrip=data/dnsping_"${what}".rrd:rtt:AVERAGE \
-  DEF:packetloss=data/dnsping_"${what}".rrd:pl:AVERAGE \
+  DEF:roundtrip=data/dnsping_"${resolver}".rrd:rtt:AVERAGE \
+  DEF:packetloss=data/dnsping_"${resolver}".rrd:pl:AVERAGE \
   CDEF:PLNone=packetloss,0,2,LIMIT,UN,UNKN,INF,IF \
   CDEF:PL2=packetloss,2,8,LIMIT,UN,UNKN,INF,IF \
   CDEF:PL15=packetloss,8,15,LIMIT,UN,UNKN,INF,IF \
@@ -132,14 +148,14 @@ for what in $h; do
   AREA:PL100#ff0000:"75-100%":STACK \
   COMMENT:"(Packet Loss Percentage)"
 
-  rrdtool graph images/dnsping_"${what}"_year.png \
+  rrdtool graph images/dnsping_"${resolver}"_year.png \
   --imginfo '<IMG SRC=/stats/%s WIDTH=%lu HEIGHT=%lu >' \
   --lazy --start -31536000 --end -86400 -h 500 -w 1200 -a PNG \
   -v "Response Time (ms)" \
   --rigid \
   --lower-limit 0 \
-  DEF:roundtrip=data/dnsping_"${what}".rrd:rtt:AVERAGE \
-  DEF:packetloss=data/dnsping_"${what}".rrd:pl:AVERAGE \
+  DEF:roundtrip=data/dnsping_"${resolver}".rrd:rtt:AVERAGE \
+  DEF:packetloss=data/dnsping_"${resolver}".rrd:pl:AVERAGE \
   CDEF:PLNone=packetloss,0,2,LIMIT,UN,UNKN,INF,IF \
   CDEF:PL2=packetloss,2,8,LIMIT,UN,UNKN,INF,IF \
   CDEF:PL15=packetloss,8,15,LIMIT,UN,UNKN,INF,IF \
